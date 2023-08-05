54c2b27d2378954d634e9fc1e2b764f5c6e0cc5b
dvcandag <dvcandag@gmail.com>
2023-08-04 21:02:55 -0500

    avance 2

--- certificado.php
+++ certificado.php
@@ -17,29 +17,15 @@ if (isset($_GET['id'])) {
     $sentencia->execute();
     $conductor = $sentencia->fetch(PDO::FETCH_ASSOC);
 
-/////////////
-
-// Consulta para obtener la fecha_caducidad de la tabla fecha_registro por ID
-$consultaFechaRegistro = "SELECT fecha_caducidad FROM fecha_registro WHERE id_conductor = :id";
-$sentenciaFechaRegistro = $conexion->prepare($consultaFechaRegistro);
-$sentenciaFechaRegistro->bindParam(':id', $idConductor, PDO::PARAM_INT);
-$sentenciaFechaRegistro->execute();
-$fechaCaducidad = $sentenciaFechaRegistro->fetchColumn();
-
-
-
-
-///////////////////
-
-
-
+    // Consulta para obtener la fecha_caducidad de la tabla fecha_registro por ID
+    $consultaFechaRegistro = "SELECT fecha_caducidad FROM fecha_registro WHERE id_conductor = :id";
+    $sentenciaFechaRegistro = $conexion->prepare($consultaFechaRegistro);
+    $sentenciaFechaRegistro->bindParam(':id', $idConductor, PDO::PARAM_INT);
+    $sentenciaFechaRegistro->execute();
+    $fechaCaducidad = $sentenciaFechaRegistro->fetchColumn();
 
     // Verificar si se encontró el conductor
     if ($conductor) {
-
-
-
-
         // Incluir la librería TCPDF
         require('libreria_qr/TCPDF/tcpdf.php');
 
@@ -48,62 +34,63 @@ $fechaCaducidad = $sentenciaFechaRegistro->fetchColumn();
 
         // Agregar una página al PDF
         $pdf->AddPage();
-// Calcular la posición X para centrar la imagen en la página A4
-$posicionX = (297 - 45) / 2; //divide pagina para centrar
 
-// Agregar el logo centrado en el PDF con la clase "logo" 25=subir
-$pdf->Image('estilos/logo/logo_muni.png', $posicionX, 25, 45, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, 'C', false, false);
+        // Calcular la posición X para centrar el logo en la página A4
+        $posicionX = (297 - 45) / 2; //divide pagina para centrar
 
+        // Agregar el logo centrado en el PDF con la clase "logo" 25=subir
+        $pdf->Image('estilos/logo/logo_muni.png', $posicionX, 25, 45, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, 'C', false, false);
 
-      // Calcular el ancho del texto "Subgerencia de Transporte"
-$anchoTexto = $pdf->GetStringWidth('Subgerencia de Transporte');
+        // Calcular el ancho del texto "Subgerencia de Transporte"
+        $anchoTexto = $pdf->GetStringWidth('Subgerencia de Transporte');
 
-// Calcular la posición X para centrar el texto en la página A4
-$posicionX = (297 - $anchoTexto) / 2;
+        // Calcular la posición X para centrar el texto en la página A4
+        $posicionX = (297 - $anchoTexto) / 2;
 
-// Agregar la Subgerencia de Transporte centrada horizontalmente en el PDF con la clase "subgerencia"
-$pdf->SetFont('helvetica', 'B', 20);
-$pdf->SetXY($posicionX, 80); // Ajusta la posición Y (50 en este caso) según tus necesidades
-$pdf->Cell($anchoTexto, 10, 'SUB-GERENCIA DE TRANSPORTES', 0, 1, 'C');
-
+        // Agregar la Subgerencia de Transporte centrada horizontalmente en el PDF con la clase "subgerencia"
+        $pdf->SetFont('helvetica', 'B', 20);
+        $pdf->SetXY($posicionX, 80); // Ajusta la posición Y (50 en este caso) según tus necesidades
+        $pdf->Cell($anchoTexto, 10, 'SUB-GERENCIA DE TRANSPORTES', 0, 1, 'C');
 
         // Agregar espaciado
         $pdf->Ln(10);
 
-
-
-
         // Agregar la información del conductor con la clase "informacion"
-$pdf->SetFont('helvetica', '', 16);
-$contenidoCertificado = strtoupper( $conductor['nombre'] . " " . 
-                       $conductor['apellido_paterno'] . " " . 
-                       $conductor['apellido_materno']) . "\n";
+        $pdf->SetFont('helvetica', '', 16);
+        $contenidoCertificado = strtoupper($conductor['nombre'] . " " . $conductor['apellido_paterno'] . " " . $conductor['apellido_materno']) . "\n";
 
+        // Agregar el espaciado deseado
+        $contenidoCertificado .= "\n"; // Puedes ajustar la cantidad de saltos de línea según tu preferencia
+        $contenidoCertificado .= "PLACA: " . $conductor['nro_placa'] . "\nMARCA: " .
+            $conductor['marca'] . " \nMODELO: " .
+            $conductor['modelo'];
 
+        $contenidoCertificado .= "\n";
+        $contenidoCertificado .= "\nCADUCIDAD: " . $fechaCaducidad;
 
- // Agregar el espaciado deseado
-$contenidoCertificado .= "\n"; // Puedes ajustar la cantidad de saltos de línea según tu preferencia
+        $pdf->MultiCell(0, 10, $contenidoCertificado, 0, 'C');
 
-       
+        // Construir el nombre del archivo QR con el formato "dni.png"
+        $nombreArchivoQR = $conductor['dni'] . ".png";
 
-$contenidoCertificado .= "PLACA: " . $conductor['nro_placa'] . "\nMARCA: " .
-                        $conductor['marca'] . " \nMODELO: " . 
-                        $conductor['modelo'];
+        // Verificar si el archivo QR existe antes de mostrarlo
+        $rutaArchivoQR = "qr_conductor/{$nombreArchivoQR}";
+        if (file_exists($rutaArchivoQR)) {
+            // Mostrar la imagen del código QR en el PDF
+            $pdf->Image($rutaArchivoQR, 25, 138, 50, 50, 'PNG');
+        } else {
+            echo "Error: No se encontró el código QR para el DNI: {$conductor['dni']}";
+        }
 
+// Verificar si el archivo de la firma del gerente existe
+$rutaArchivoFirmaGerente = "estilos/firma/firma_gerente.jpg";
+if (file_exists($rutaArchivoFirmaGerente)) {
+    // Mostrar la imagen de la firma del gerente en el PDF
+    $pdf->Image($rutaArchivoFirmaGerente, 220, 158, 50, 0, 'JPG'); // Cambiar a 'JPEG' si el formato es JPG
+} else {
+    echo "Error: No se encontró la firma del gerente.";
+}
 
-
-
-$contenidoCertificado .= "\n"; 
-                       $contenidoCertificado .= "\nCADUCIDAD: " . $fechaCaducidad;
-
-
-$pdf->MultiCell(0, 10, $contenidoCertificado, 0, 'C');
-
-/////////////////
-
-
-
-/////////////////////////
         // Generar el PDF
         ob_clean(); // Limpiamos cualquier salida previa
         $pdf->Output();
@@ -116,4 +103,6 @@ $pdf->MultiCell(0, 10, $contenidoCertificado, 0, 'C');
     // Si no se recibió el parámetro 'id' en la URL, puedes mostrar un mensaje de error o redireccionar a otra página.
     echo "Error: Falta el parámetro 'id' en la URL.";
 }
+
+
 ?>
--- generaqr.php
+++ generaqr.php
@@ -2,23 +2,12 @@
 // Realiza la conexión a la base de datos
 require_once "config/connection.php";
 
+// Incluir la librería PHPQRCode
+require_once "libreria_qr/PHPQRCode/qrlib.php";
+
 // Función para generar el código QR y guardar la imagen en la carpeta "qr_conductor"
-function generarQR($datos, $filename)
+function generarQR($conexion, $datos, $filename)
 {
-    // Tamaño del código QR y margen
-    $longitud = 5;
-    $margen = 1;
-
-    // Creamos una imagen en blanco del tamaño del código QR
-    $imagen = imagecreate($longitud * 25, $longitud * 25);
-
-    // Definimos los colores para el código QR
-    $colorBlanco = imagecolorallocate($imagen, 255, 165, 0);
-    $colorNegro = imagecolorallocate($imagen, 0, 0, 0);
-
-    // Rellenamos la imagen con el color blanco
-    imagefill($imagen, 0, 0, $colorBlanco);
-
     // Construir el contenido del código QR con los datos del conductor
     $contenidoQR = "DNI: {$datos['dni']}\n";
     $contenidoQR .= "Nombre: {$datos['apellido_paterno']} {$datos['apellido_materno']}, {$datos['nombre']}\n";
@@ -30,37 +19,29 @@ function generarQR($datos, $filename)
     $fechaCaducidad = date('Y-m-d', strtotime('+2 years', strtotime($datos['fecha_registro'])));
     $contenidoQR .= "Caducidad: {$fechaCaducidad}\n";
 
-    // Convertimos el contenido en una matriz de puntos (0 para blanco, 1 para negro)
-    $puntos = str_split($contenidoQR);
-    $x = $margen;
+    // Generar el código QR con la cadena de datos
+    QRcode::png($contenidoQR, "qr_conductor/{$filename}", 'L', 10, 2);
 
-    // Dibujamos el código QR punto por punto
-    foreach ($puntos as $punto) {
-        $y = $margen;
-        if ($punto === "2") {
-            imagesetpixel($imagen, $x, $y, $colorNegro);
-        }
-        $x += $longitud;
-    }
-
-    // Guardamos la imagen como archivo PNG en la carpeta "qr_conductor"
-    $rutaImagenQR = "qr_conductor/" . $filename;
-    imagepng($imagen, $rutaImagenQR);
-
-    // Liberamos memoria
-    imagedestroy($imagen);
-
-    // Actualizamos la fecha de caducidad en la base de datos
+    // Actualizar la fecha de caducidad en la base de datos
     $idConductor = $datos['id_conductor'];
-    $fechaCaducidad = date('Y-m-d', strtotime('+2 years', strtotime($datos['fecha_registro'])));
+    $fechaGeneracionQR = date('Y-m-d H:i:s'); // Fecha y hora actual
 
-    // Establecer la conexión a la base de datos (ya que estamos dentro de una función)
-    global $conexion;
+    try {
+        // Iniciar la transacción
+        $conexion->beginTransaction();
 
-    // Consulta para actualizar la fecha de caducidad en la tabla "fecha_registro"
-    $actualizarFechaCaducidad = "UPDATE fecha_registro SET fecha_caducidad = :fechaCaducidad WHERE id_conductor = :idConductor";
-    $sentenciaActualizar = $conexion->prepare($actualizarFechaCaducidad);
-    $sentenciaActualizar->execute(['fechaCaducidad' => $fechaCaducidad, 'idConductor' => $idConductor]);
+        // Consulta para actualizar la fecha de caducidad y la fecha de generación del QR en la tabla "fecha_registro"
+        $actualizarFechaQR = "UPDATE fecha_registro SET fecha_caducidad = :fechaCaducidad, fecha_certificado_qr = NOW() WHERE id_conductor = :idConductor";
+        $sentenciaActualizarQR = $conexion->prepare($actualizarFechaQR);
+        $sentenciaActualizarQR->execute(['fechaCaducidad' => $fechaCaducidad, 'idConductor' => $idConductor]);
+
+        // Confirmar la transacción
+        $conexion->commit();
+    } catch (PDOException $e) {
+        // Si ocurre algún error, deshacer la transacción
+        $conexion->rollBack();
+        echo "Error al actualizar la fecha de caducidad en la base de datos: " . $e->getMessage();
+    }
 }
 
 // Consulta para obtener los datos de conductores y vehículos
@@ -80,14 +61,18 @@ if (!empty($registros)) {
         $nombreArchivoQR = $registro['dni'] . ".png";
 
         // Generar y guardar el código QR con los datos del conductor
-        generarQR($registro, $nombreArchivoQR);
+        generarQR($conexion, $registro, $nombreArchivoQR);
+
+        // Calcular la fecha de caducidad para mostrarla
+        $fechaCaducidad = date('d/m/Y', strtotime('+2 years', strtotime($registro['fecha_registro'])));
 
         // Mostrar el contenido del código QR para verificar su contenido (opcional)
-        echo "Contenido del código QR para DNI: {$registro['dni']} <br>";
-       
+        echo "Código QR para DNI: {$registro['dni']} <br>";
+        echo nl2br($registro['dni'] . "\n" . $registro['nombre'] . "\n" . $registro['celular'] . "\n" . $registro['nro_placa'] . "\n" . $registro['marca'] . "\n" . $registro['modelo'] . "\n" . "Caducidad: {$fechaCaducidad}" . "\n\n");
     }
 
     echo "Se han generado y guardado los códigos QR exitosamente.";
 } else {
     echo "No hay registros para generar códigos QR.";
 }
+?>
